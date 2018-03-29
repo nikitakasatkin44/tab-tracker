7775ab2b962911953b5d639d021b7d6fe6d187c3
ng.kasatkin <ng.kasatkin@tensor.ru>
2018-03-29 21:14:01 +0300

    backend init

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello Nick!'
+    })
+});
+
+console.log('App listening of port 8081');
+app.listen(process.env.PORT || 8081);
